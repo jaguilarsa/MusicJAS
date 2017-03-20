5a9a939e593208853600924116fea830fc333f2e
botika <aritemeeul@gmail.com>
2017-03-20 01:00:50 +0000

    Client initial commit

---
+++ client/src/app/views/main/main.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'main-view',
+    templateUrl: 'main.component.html',
+    styleUrls: ['main.component.scss']
+})
+class MainViewComponent {
+
+}
