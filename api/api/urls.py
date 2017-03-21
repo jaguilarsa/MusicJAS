76bd75268b4ca993140ef70113157da7322f2adc
botika <aritemeeul@gmail.com>
2017-03-21 00:42:06 +0000

    Disable admin api

--- api/api/urls.py
+++ api/api/urls.py
@@ -1,7 +1,5 @@
 from django.conf.urls import url, include
-from django.contrib import admin
 
 urlpatterns = [
-    url(r'^', include('core.urls')),
-    url(r'^admin/', admin.site.urls)
+    url(r'^', include('core.urls'))
 ]
