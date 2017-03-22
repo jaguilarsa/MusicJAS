837a55ca9eeca544e40d5c64e56ec2a0bd22b4ed
botika <aritemeeul@gmail.com>
2017-03-22 08:40:04 +0000

    First implementation data table and fix minor issues

--- client/src/app/views/main/main.component.ts
+++ client/src/app/views/main/main.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit, ViewChild, AfterViewInit, ChangeDetectionStrategy } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
 import { MdSidenav } from '@angular/material/sidenav';
 import { Http, Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/switch';
 import 'rxjs/add/observable/fromPromise';
 
 interface ListData {
@@ -20,15 +20,20 @@ interface TrackData {
 const API_URL: string = 'http://localhost:8000/';
 
 @Component({
-    selector:        'main-view',
-    templateUrl:     'main.component.html',
-    styleUrls:       ['main.component.scss']
+    selector:    'main-view',
+    templateUrl: 'main.component.html',
+    styleUrls:   ['main.component.scss']
 })
-export class MainViewComponent implements AfterViewInit, OnInit {
+export class MainViewComponent implements OnInit {
 
     public chartData: Observable<any[]>;
     public mainData: Observable<ListData[]>;
     public listData: Observable<TrackData[]>;
+
+    public rows = [];
+    public count: number = 0;
+    public offset: number = 0;
+    public limit: number = 10;
 
     @ViewChild('right') private right: MdSidenav;
     @ViewChild('left') private left: MdSidenav;
@@ -36,14 +41,12 @@ export class MainViewComponent implements AfterViewInit, OnInit {
     constructor(private http: Http) {
     }
 
-    public ngAfterViewInit() {
-        console.log('Run View');
-        setTimeout(this.left.open(), 1000);
-    }
-
     public ngOnInit() {
+        // Fix changes sidenav change issue
+        setTimeout(this.left.open(), 1000);
         this.mainData = this.http.get(`${API_URL}genre/`)
         .map((res: Response): ListData[] => res.json());
+        // this.page(this.offset, this.limit);
     }
 
     public onClick(id: string) {
@@ -51,6 +54,39 @@ export class MainViewComponent implements AfterViewInit, OnInit {
         .map((res: Response) => res.json()
             .map((data) => [data.album__artist__name, data.duration__avg])
         );
+    }
+
+    public page(offset, limit) {
+        this.fetch((results) => {
+            this.count = results.length;
+
+            const start = offset * limit;
+            const end = start + limit;
+            const rows = [...this.rows];
+
+            for (let i = start; i < end; i++) {
+                rows[i] = results[i];
+            }
+
+            this.rows = rows;
+            console.log('Page Results', start, end, rows);
+        });
+    }
+
+    public fetch(cb) {
+        const req = new XMLHttpRequest();
+        req.open('GET', API_URL + 'track/?genre=1');
+
+        req.onload = () => {
+            cb(JSON.parse(req.response));
+        };
+
+        req.send();
+    }
+
+    public onPage(event) {
+        console.log('Page Event', event);
+        this.page(event.offset, event.limit);
     }
 
 }
--- client/src/app/views/main/main.module.ts
+++ client/src/app/views/main/main.module.ts
@@ -2,14 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MdSidenavModule } from '@angular/material/sidenav';
 import { MdListModule } from '@angular/material/list';
-
 import { MainViewComponent } from './main.component';
 import { ChartComponent } from '../../components/chart/chart.component';
+import { Angular2DataTableModule } from 'angular2-data-table';
 
 @NgModule({
-    imports: [CommonModule, MdSidenavModule, MdListModule],
+    imports:      [CommonModule, MdSidenavModule, MdListModule, Angular2DataTableModule],
     declarations: [MainViewComponent, ChartComponent],
-    exports: [MainViewComponent]
+    exports:      [MainViewComponent]
 })
 export class MainViewModule {
 }
