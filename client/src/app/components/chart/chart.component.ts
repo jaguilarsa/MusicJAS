b9cbe35b0948b4161a4a98bc6c9e9331827c9962
botika <aritemeeul@gmail.com>
2017-03-23 16:56:32 +0000

    add responsive feature; fix issues

--- client/src/app/components/chart/chart.component.ts
+++ client/src/app/components/chart/chart.component.ts
@@ -1,13 +1,16 @@
 import {
-    Component, OnChanges, ViewChild, ElementRef, Input, ChangeDetectionStrategy, Inject, AfterViewInit, OnDestroy,
-    SimpleChanges, OnInit
+    Component, OnChanges, ViewChild, ElementRef, Input, ChangeDetectionStrategy, OnDestroy, SimpleChanges, OnInit
 } from '@angular/core';
-import { DOCUMENT } from '@angular/platform-browser';
 import * as d3 from 'd3';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/fromEvent';
 import 'rxjs/add/operator/do';
 import { Subscription } from 'rxjs';
+
+export interface ChartData {
+    millis: number;
+    label: string;
+}
 
 @Component({
     selector:        'chart',
@@ -17,48 +20,54 @@ import { Subscription } from 'rxjs';
 })
 export class ChartComponent implements OnInit, OnChanges, OnDestroy {
     @ViewChild('chart') private chartContainer: ElementRef;
-    @Input() private data;
+    @Input('data') private data: ChartData[];
 
-    private resizeS: Subscription;
+    private subs: Subscription[] = [];
 
-    private margin: any = { top: 80, bottom: 80, left: 160, right: 80 };
-    private chart: any;
+    private margin: any = { top: 80, bottom: 80, left: 120, right: 80 };
     private width: number;
     private height: number;
+    private colors: any;
+
     private xScale: any;
     private yScale: any;
-    private colors: any;
+
+    private xDomain: number[];
+    private yDomain: string[];
+
+    private chart: any;
     private xAxis: any;
     private yAxis: any;
     private svg: any;
 
     public ngOnInit() {
-        this.resizeS = Observable.fromEvent(window, 'resize').map(() => {
+        this.subs.push(Observable.fromEvent(window, 'resize').map(() => {
             if (this.data && this.chartContainer) {
-                this.createChart();
-                this.updateChart();
+                this.resize();
             }
-        }).subscribe();
+        }).subscribe());
     }
 
     public ngOnChanges(changes: SimpleChanges) {
-        if (this.chartContainer && changes.data.currentValue) {
-            if (!this.chart) {
-                this.createChart();
+        if (this.chartContainer && this.data) {
+            if (this.chart) {
+                this.update();
+            } else {
+                this.create();
             }
-            this.updateChart();
         }
     }
 
     public ngOnDestroy() {
-        this.resizeS.unsubscribe();
-
+        this.subs.forEach((sub) => {
+            sub.unsubscribe();
+        });
     }
 
-    private createChart() {
-        const element = this.chartContainer.nativeElement;
-        this.width = element.offsetWidth - this.margin.left - this.margin.right;
-        this.height = element.offsetHeight - this.margin.top - this.margin.bottom;
+    private create() {
+
+        this.setDomain();
+        const element: HTMLDivElement = this.setSize();
         this.svg = d3.select(element).append('svg')
         .attr('width', element.offsetWidth)
         .attr('height', element.offsetHeight);
@@ -67,13 +76,6 @@ export class ChartComponent implements OnInit, OnChanges, OnDestroy {
         this.chart = this.svg.append('g')
         .attr('class', 'bars')
         .attr('transform', `translate(${this.margin.left}, ${this.margin.top})`);
-
-        // define X & Y domains
-        let xDomain = [0, +d3.max(this.data, (d) => d[1])];
-        let yDomain = this.data.slice(0).map((d) => d[0]);
-        // create scales
-        this.xScale = d3.scaleTime().domain(xDomain).range([this.width, 0]);
-        this.yScale = d3.scaleBand().padding(0.1).domain(yDomain).rangeRound([0, this.height]);
 
         // bar colors
         this.colors = d3.scaleLinear()
@@ -89,40 +91,46 @@ export class ChartComponent implements OnInit, OnChanges, OnDestroy {
         .attr('class', 'axis axis-y')
         .attr('transform', `translate(${this.margin.left}, ${this.margin.top})`)
         .call(d3.axisLeft(this.yScale));
+        this.update();
+    }
+
+    private setDomain() {
+        this.xDomain = [0, +d3.max(this.data, (d: ChartData): number => d.millis)];
+        this.yDomain = this.data.slice(0).map((d: ChartData): string => d.label);
+    }
+
+    private setSize(): HTMLDivElement {
+        const element: HTMLDivElement = this.chartContainer.nativeElement;
+        this.width = element.offsetWidth - this.margin.left - this.margin.right;
+        this.height = element.offsetHeight - this.margin.top - this.margin.bottom;
+        this.xScale = d3.scaleTime().domain(this.xDomain).range([0, this.width]);
+        this.yScale = d3.scaleBand().padding(0.1).domain(this.yDomain).rangeRound([0, this.height]);
+        return element;
     }
 
     private resize() {
-        let element = this.chartContainer.nativeElement;
-        this.width = element.offsetWidth - this.margin.left - this.margin.right;
-        this.height = element.offsetHeight - this.margin.top - this.margin.bottom;
+        this.setDomain();
+        const element: HTMLDivElement = this.setSize();
+        this.svg = this.svg
+        .attr('width', element.offsetWidth)
+        .attr('height', element.offsetHeight);
+
         this.xAxis = this.xAxis
         .attr('x', 0)
         .attr('y', 0)
         .attr('transform', `translate(${this.margin.left}, ${this.margin.top + this.height})`)
+        .call(d3.axisBottom(this.xScale).tickFormat(d3.timeFormat('%M:%S')));
+
         this.yAxis = this.yAxis
         .attr('x', 0)
         .attr('y', 0)
         .attr('transform', `translate(${this.margin.left}, ${this.margin.top})`)
-        this.chart.selectAll('.bar').transition()
-        .attr('x', (d) => this.xScale(d[1]))
-        .attr('y', (d) => this.yScale(d[0]))
-        .attr('width', (d) => this.width - this.xScale(d[1]))
-        .attr('height', (d) => this.yScale.bandwidth())
-        .style('fill', (d, i) => this.colors(i));
-        this.svg = this.svg
-        .attr('width', element.offsetWidth)
-        .attr('height', element.offsetHeight);
+        .call(d3.axisLeft(this.yScale));
+        this.updateBars();
     }
 
-    private updateChart() {
-        // update scales & axis
-        this.xScale.domain([0, +d3.max(this.data, (d) => d[1])]);
-        this.yScale.domain(this.data.slice(0).map((d) => d[0]));
-        this.colors.domain([0, this.data.length]);
-        this.xAxis.transition().call(d3.axisBottom(this.xScale).tickFormat(d3.timeFormat('%M:%S')));
-        this.yAxis.transition().call(d3.axisLeft(this.yScale));
-
-        let update = this.chart.selectAll('.bar')
+    private updateBars() {
+        const update = this.chart.selectAll('.bar')
         .data(this.data);
 
         // remove exiting bars
@@ -130,25 +138,38 @@ export class ChartComponent implements OnInit, OnChanges, OnDestroy {
 
         // update existing bars
         this.chart.selectAll('.bar').transition()
-        .attr('x', (d) => this.xScale(d[1]))
-        .attr('y', (d) => this.yScale(d[0]))
-        .attr('width', (d) => this.width - this.xScale(d[1]))
-        .attr('height', (d) => this.yScale.bandwidth())
+        .attr('x', 0)
+        .attr('y', (d) => this.yScale(d.label))
+        .attr('width', (d: ChartData) => this.xScale(d.millis))
+        .attr('height', (d: ChartData) => this.yScale.bandwidth())
         .style('fill', (d, i) => this.colors(i));
+        return update;
+    }
+
+    private update() {
+        this.setDomain();
+        // update scales & axis
+        this.xScale.domain(this.xDomain);
+        this.yScale.domain(this.yDomain);
+        this.colors.domain([0, this.data.length]);
+        this.xAxis.transition().call(d3.axisBottom(this.xScale).tickFormat(d3.timeFormat('%M:%S')));
+        this.yAxis.transition().call(d3.axisLeft(this.yScale));
+
+        const update = this.updateBars();
 
         // add new bars
         update
         .enter()
         .append('rect')
         .attr('class', 'bar')
-        .attr('x', (d) => this.xScale(d[1]))
-        .attr('y', (d) => this.yScale(d[0]))
+        .attr('x', 0)
+        .attr('y', (d: ChartData) => this.yScale(d.label))
         .attr('width', 0)
         .attr('height', this.yScale.bandwidth())
         .style('fill', (d, i) => this.colors(i))
         .transition()
         .delay((d, i) => i * 10)
-        .attr('y', (d) => this.yScale(d[0]))
-        .attr('width', (d) => this.width - this.xScale(d[1]));
+        .attr('y', (d: ChartData) => this.yScale(d.label))
+        .attr('width', (d: ChartData) => this.xScale(d.millis))
     }
 }
